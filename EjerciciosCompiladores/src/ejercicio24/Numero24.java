001b70d90985da2a674641de4200b47ffbbf2ef1
ozzie238s <ozzIE@10.20.93.125>
2015-05-13 14:29:24 -0600

    Copiar y pegar ejercicios 23 y 24 de repositorio previo https://github.com/ozzie238s/52Programas

--- EjerciciosCompiladores/src/ejercicio24/Numero24.java
+++ EjerciciosCompiladores/src/ejercicio24/Numero24.java
@@ -5,10 +5,36 @@
  */
 package ejercicio24;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ozzIE
  */
 public class Numero24 {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ingrese un numero a revisar si es primo(Mayor que cero):");
+        int x=scan.nextInt();
+        while(x<=0)
+        {System.out.println("Ingrese otro numero, recuerde que debe ser mayor que zero:");
+        x=scan.nextInt();
+        }
+        int flag=0;
+        for(int i=2;i<x;i++)
+           {
+               if(x%i==0)
+                 {
+                   flag=1;
+                 }
+         
+           }
+           if(flag==1)
+            System.out.println("Ese numero no es primo");
+           else
+            System.out.println("Ese numero si es primo");   
+        
+        }
     
 }
