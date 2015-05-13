3cf198b2ecfe1dfe46cbbc81cc36084c3805ae5e
ozzie238s <ozzIE@10.20.93.125>
2015-05-13 15:28:02 -0600

    Agregando ejercicios 27 y 28

--- EjerciciosCompiladores/src/ejercicio27/Numero27.java
+++ EjerciciosCompiladores/src/ejercicio27/Numero27.java
@@ -5,10 +5,30 @@
  */
 package ejercicio27;
 
+import java.util.Scanner;
+
 /**
- *
+ * Ejercicio numero 27: Es una tabla de multiplicacion del 0 al 10
+ * 
+ * @since 13/5/2015
  * @author ozzIE
+ * @version 1.0
  */
 public class Numero27 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int x,y,z;
+        System.out.println("Ingrese el numero para ver tabla de multiplicacion");
+        x= scan.nextInt();
+        while(x<0||x>10)
+        {System.out.println("Ese numero no esta en esta tabla porfavor ingrese otro numero");
+        x= scan.nextInt();
+        }
+          for(int i=0;i<11;i++)
+             {
+              y=i*x;
+              System.out.println(x+"x"+i+"="+y+"\n");
+             }
+    }
 }
--- EjerciciosCompiladores/src/ejercicio28/Numero28.java
+++ EjerciciosCompiladores/src/ejercicio28/Numero28.java
@@ -5,10 +5,41 @@
  */
 package ejercicio28;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
- *
+ * Ejercicio numero 28: Indica si una moneda callo en aguila o sello
+ * 
+ * @since 13/5/2015
  * @author ozzIE
+ * @version 1.0
  */
 public class Numero28 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Ingrese 'Y' si desea voltear la moneda o cualquier otra tecla para salir");
+        String option= scan.nextLine();
+        String moneda;
+        while(option.equals("Y")||option.equals("y") )
+        {
+            int x=random.nextInt();
+            if(x%2==0)
+            moneda="Aguila";
+            else
+            {moneda="Sello";}
+            System.out.println("La moneda callo en: "+moneda);
+            System.out.println("Ingrese 'Y' si desea voltear la moneda o cualquier otra tecla para salir");
+            option= scan.nextLine();
+        }
+        
+        
+        
+        
+        
+        
+    }
+    
     
 }
